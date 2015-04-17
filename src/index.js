60a8d40384541d48c6cff405c45e7ad9861143c7
yosh <yoshtalwar@gmail.com>
2015-04-17 15:34:17 +0100

    cleaned up build

--- src/index.js
+++ src/index.js
@@ -1,15 +1,2 @@
-var AgeGate = require('agegate');
-
-var gate = new AgeGate({
-  form: document.querySelector('form'),
-  countries: true
-}, function(err) {
-  if (err)
-    alert('Either you\'re too young, or you messed with the form');
-  else
-    window.location = 'https://github.com/izolate/agegate';
-});
-
-document.addEventListener('DOMContentLoaded', function(e) {
-  gate.render();
-});
+// import AgeGate and bind to window scope
+window.AgeGate = require('agegate');
