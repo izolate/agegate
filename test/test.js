c8992e140b1b8c5fb3697d9d17dbe071cee47190
yosh <yoshtalwar@gmail.com>
2015-04-27 15:27:45 +0100

    fixed test to go with variable name change

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ describe('AgeGate', function() {
   var form = document.querySelector('form');
 
   describe('Instantiate class', function() {
-     var gate = new AgeGate({
+    var gate = new AgeGate({
       form: form,
       expiry: Infinity,
       countries: true,
@@ -12,10 +12,10 @@ describe('AgeGate', function() {
     }, null);
 
     it('should save options', function() {
-      assert.equal(gate.defaults.form, form);
-      assert.strictEqual(gate.defaults.expiry, Infinity);
-      assert.strictEqual(gate.defaults.countries, true);
-      assert.strictEqual(gate.defaults.age, 21);
+      assert.equal(gate.options.form, form);
+      assert.strictEqual(gate.options.expiry, Infinity);
+      assert.strictEqual(gate.options.countries, true);
+      assert.strictEqual(gate.options.age, 21);
     });
 
     it('should override default age with supplied age', function() {
